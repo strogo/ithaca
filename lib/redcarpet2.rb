528e474b22b02f468842bc954d5c459b35bc35d4
Peter Aronoff <telemachus@arpinum.org>
2011-11-25 15:25:24 -0500

    Fix smartypants

--- lib/redcarpet2.rb
+++ lib/redcarpet2.rb
@@ -6,13 +6,16 @@ class RedCarpet2 < Nanoc3::Filter
 
     options = params[:options] || []
     smart = options.delete(:smart)
+    markdown = Redcarpet::Markdown.new(::Redcarpet::Render::HTML, *options)
 
     if smart
-      html =
-        Redcarpet::Markdown.new(::Redcarpet::Render::HTML, *options).render(content)
+      # This should be temporary for 2.0.0b5
+      # See https://github.com/tanoku/redcarpet/issues/57
+      html = markdown.render(content)
+      html.gsub!("&#39;", "'")
       Redcarpet::Render::SmartyPants.render(html)
     else
-      Redcarpet::Markdown.new(::Redcarpet::Render::HTML, *options).render(content)
+      markdown.render(content)
     end
   end
 end
