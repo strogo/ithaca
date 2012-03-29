f04a60eb8452ed4e1a3c7d65e90745f0f032b573
Peter Aronoff <telemachus@arpinum.org>
2012-03-29 14:29:49 -0400

    Require the rendering helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,4 +1,5 @@
 include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Rendering
 require 'fileutils'
 require 'redcarpet'
 
@@ -8,4 +9,12 @@ def blog_route(item)
   end
   year, name = item.identifier[0..11], item.identifier[12..-1]
   year.gsub('-', '/') + name.chop + '.html'
+end
+
+def item_url(item, site=@site.config[:base_url])
+  [site,  blog_route(item)].join
+end
+
+def item_title(item)
+  item.attributes[:title] || "(No title - man, that's odd)"
 end
